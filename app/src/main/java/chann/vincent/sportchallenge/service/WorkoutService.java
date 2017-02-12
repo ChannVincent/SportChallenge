b7e2bd6354236a222c0c233509a9da44e6a3bacb
ChannVincent <vincent.chann.engineer@gmail.com>
2017-02-12 19:34:53 +0100

    SC-03 : développement du workout service : gestion des audios

--- app/src/main/java/chann/vincent/sportchallenge/service/WorkoutService.java
+++ app/src/main/java/chann/vincent/sportchallenge/service/WorkoutService.java
@@ -8,7 +8,13 @@ import android.support.v7.app.NotificationCompat;
 import android.util.Log;
 import android.widget.RemoteViews;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import chann.vincent.sportchallenge.R;
+import fr.smartapps.lib.SMAAssetManager;
+import fr.smartapps.lib.audio.SMAAudioPlayer;
+import fr.smartapps.lib.audio.SMAAudioPlayerListener;
 
 /**
  * Created by vincentchann on 12/02/2017.
@@ -19,10 +25,30 @@ public class WorkoutService extends Service {
     private String TAG = "WorkoutService";
     protected WorkoutBinder workoutBinder = new WorkoutService.WorkoutBinder();
     protected WorkoutServiceListener listener = null;
+    protected SMAAssetManager assetManager;
+    protected SMAAudioPlayer audioPlayerMusic;
+    protected SMAAudioPlayer audioPlayerCheers;
+    protected List<String> audioFileList = new ArrayList<>();
+    protected int currentCheerPosition = 0;
 
     /*
     Life cycle
      */
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        assetManager = new SMAAssetManager(this);
+        assetManager.setDefaultStorageType(SMAAssetManager.STORAGE_TYPE_ASSETS);
+        assetManager.setExtensionDirectory("shaun_t/");
+        audioFileList.add("media01.mp3");
+        audioFileList.add("media02.mp3");
+        audioFileList.add("media03.mp3");
+        audioFileList.add("media04.mp3");
+        audioFileList.add("media05.mp3");
+        audioFileList.add("media06.mp3");
+        audioFileList.add("media07.mp3");
+    }
+
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         String action = intent.getAction();
@@ -56,24 +82,34 @@ public class WorkoutService extends Service {
     protected void startActionPlay() {
         if (listener != null) {
             listener.play();
+            startMusic("music01.mp3");
         }
     }
 
     protected void startActionPause() {
         if (listener != null) {
             listener.pause();
+            pauseMusic();
         }
     }
 
     protected void startActionNext() {
         if (listener != null) {
             listener.next();
+            if (currentCheerPosition < (audioFileList.size() - 1)) {
+                currentCheerPosition++;
+            }
+            startCheer(audioFileList.get(currentCheerPosition));
         }
     }
 
     protected void startActionPrevious() {
         if (listener != null) {
             listener.previous();
+            if (currentCheerPosition > 0) {
+                currentCheerPosition--;
+            }
+            startCheer(audioFileList.get(currentCheerPosition));
         }
     }
 
@@ -103,6 +139,50 @@ public class WorkoutService extends Service {
 
         // show notification
         startForeground(NotificationConstants.NOTIFICATION_ID.FOREGROUND_SERVICE, notification);
+    }
+
+    /*
+    Audio
+     */
+    protected void startMusic(String filename, float volume) {
+        audioPlayerMusic = assetManager.getAudioPlayer("music/" + filename, this, new SMAAudioPlayerListener() {
+            @Override
+            public void onSongProgress(int progress, int totalProgress) {
+                // callback as long as the song is playing
+            }
+
+            @Override
+            public void onSongFinish(int totalProgress) {
+                // callback when the song is finished
+            }
+        });
+        audioPlayerMusic.start();
+        audioPlayerMusic.setVolume(volume);
+    }
+
+    protected void startMusic(String filename) {
+        startMusic(filename, 0.15f);
+    }
+
+    protected void pauseMusic() {
+        audioPlayerMusic.pause();
+    }
+
+    protected void startCheer(String filename, float volume) {
+        if (audioPlayerCheers != null) {
+            audioPlayerCheers.pause();
+            audioPlayerCheers.stop();
+            audioPlayerCheers.release();
+            audioPlayerCheers = null;
+        }
+
+        audioPlayerCheers = assetManager.getAudioPlayer("cheer/" + filename, this, null);
+        audioPlayerCheers.start();
+        audioPlayerCheers.setVolume(volume);
+    }
+
+    protected void startCheer(String filename) {
+        startCheer(filename, 1f);
     }
 
     /*
